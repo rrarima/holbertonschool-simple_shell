26d91139fcc67e0a74dcddd792e6272cdafcc768
Jenny <5704@holbertonstudents.com>
2022-11-30 00:39:52 -0800

    Added working shell func that only handles executables

---
+++ pid/shell.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int ac, char **av, char **env)
+{
+	size_t n = 20;
+	size_t i;
+	char *lineptr;
+	char *token;
+
+	lineptr = NULL;
+	while (ac != 0)
+	{
+		printf("#cisfun$ ");
+		if (getline(&lineptr, &n, stdin) == -1)
+		{
+			break;
+		}
+		token = strtok(lineptr, " \t\n\r");
+		i = 0;
+		while (i < n && token != NULL)
+		{
+			av[i] = token;
+			token = strtok(NULL, " \t\n\r");
+			i = i + 1;
+		}
+		av[i] = NULL;
+		if (fork() == 0)
+		{
+			if (execve(av[0], av, env) == -1)
+			{
+				perror("./shell");
+			}
+			exit(0);
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+	return (0);
+}
