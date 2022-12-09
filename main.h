bcae5c710d91e7dfb610faeb2a6da1fe26905fe9
Jenny <5704@holbertonstudents.com>
2022-12-09 00:23:32 -0800

    Updated main header

--- main.h
+++ main.h
@@ -29,9 +29,10 @@ int _strlen(const char *s);
 int _strcmp(char *s1, char *s2);
 void print_env(void);
 listpathdir_t *ll_path(char *PATH);
-char **split_delim(char *, const char *);
+void split_delim(char *s, const char *delim, char *whole_path[]);
 void exit_func(char *lineptr);
-void parse_input(char *lineptr, char *args[], size_t n, ssize_t chars_read);
+int parse_input(char *lineptr, char *args[], ssize_t chars_read, int *exit_code);
 void read_input(char **lineptr, size_t *n, ssize_t *chars_read);
-int fork_child(char *lineptr, char *args[]);
+int fork_child(char *lineptr, char *args[], int *exit_code);
+char *access_check(listpathdir_t *head, char *argv_0);
 #endif
