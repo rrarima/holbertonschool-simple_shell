4816d948e215cc2827f915e0db60dd9f23b4f448
Jenny <5704@holbertonstudents.com>
2022-12-11 04:49:26 -0800

    Removed de-bug printfs and added free(new)

--- ll_path.c
+++ ll_path.c
@@ -1,13 +1,12 @@
 #include "main.h"
+
 listpathdir_t *ll_path(char *PATH)
 {
 	int i = 0;
 	char *whole_path[1024];
 	listpathdir_t *new, *head = NULL;
 
-	printf("This is whole path before delim: %s\n", PATH);
 	split_delim(PATH, ":", whole_path);
-	printf("Whole path: %s\n", whole_path[0]);
 	new = malloc(sizeof(*new));
 	if (new == NULL)
 	{
@@ -16,19 +15,19 @@ listpathdir_t *ll_path(char *PATH)
 	}
 	while (whole_path[i] != NULL)
 	{
-		printf("This is the whole path: %s\n", whole_path[i]);
 		new->dir = strdup(whole_path[i]);
 		new->next = head;
 		head = new;
 		i = i + 1;
 	}
+	free(new);
 	return (head);
 }
 
 
 void split_delim(char *s, const char *delim, char *whole_path[])
 {
-	char *s1, *piece, *ptr;
+	char *s1 = NULL, *piece = NULL, *ptr = NULL;
 	int i = 0, j = 0;
 
 	while (ptr == s && (ptr = strstr(ptr, delim)))
@@ -36,16 +35,10 @@ void split_delim(char *s, const char *delim, char *whole_path[])
 		i = i + 1;
 		ptr = ptr + 1;
 	}
-
-/*	result = malloc(sizeof(char *) * (i + 1));*/
-
 	s1 = strdup(s);
-
 	piece = strtok(s1, delim);
-
 	while (piece != NULL)
 	{
-		printf("This is piece: %s\n", piece);
 		whole_path[j] = piece;
 		piece = strtok(NULL, delim);
 		j = j + 1;
